fa1353ef8f68c2b0d488be0d2d1da9923440caab
khalifa5000 <m.khalifa5000@gmail.com>
2024-09-09 02:18:06 +0300

    Edit link

--- src/main.jsx
+++ src/main.jsx
@@ -4,29 +4,28 @@ import App from './App.jsx';
 import './index.css';
 import {DataProvider} from "./context/DataContext.jsx";
 
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Route,
-// } from "react-router-dom";
-// import About from './About.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './About.jsx';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App/>
-//   },
-//   {
-//     path: "/about",
-//     element: <About/>
-//   }
-// ])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/about",
+    element: <About/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router}/> */}
     <DataProvider>
-      <App />
+      {/* <App /> */}
+        <RouterProvider router={router}/>
     </DataProvider>
   </React.StrictMode>,
 )
